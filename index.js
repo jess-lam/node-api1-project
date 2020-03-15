f199b30ac110ae36446bf5004e79c6e4f0ff88c0
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-03-15 00:55:50 -0700

    created the get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// implement your API here
+const db = require('./data/db');
+
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, () => {
+  // eslint-disable-next-line no-console
+  console.log('*** Server listening on port 4000 ***');
+});
+
+
+server.use(express.json());
+
+
+server. get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+
+server.get('/users', (req, res) => {
+  db.find()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(errorMessage => {
+    res.status(500).json({sucess: false, message: "The users information could not be retrieved."});
+  });
+})
+
+
