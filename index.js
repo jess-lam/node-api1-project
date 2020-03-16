4eb383d20d8eb1f3340568b15f2d4c84dae3c5fa
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-03-15 18:15:50 -0700

    created the get, post, and delete endpoints

--- index.js
+++ index.js
@@ -23,9 +23,39 @@ server.get('/users', (req, res) => {
   .then(users => {
     res.status(200).json(users);
   })
-  .catch(errorMessage => {
-    res.status(500).json({sucess: false, message: "The users information could not be retrieved."});
+  .catch(err => {
+    res.status(500).json({success: false, err: "The users information could not be retrieved."});
   });
 })
+
+server.post('/users', (req, res) => {
+  const userInfo = req.body;
+  console.log('body:', userInfo);
+
+  db.add(userInfo)
+  .then((user) => {
+    res.status(201).json({ success: true, user});
+    .catch((err) => {
+      res.status(500.json({sucess:false, err}));
+    });
+  });
+});
+
+server.delete('/users/:id', (req, res) => {
+  const { id } = rq.params;
+  console.log('yeah');
+
+  db.remove(id)
+  .then(deletedUser => {
+    if (deletedUser) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'id not found'});
+    }
+  })
+  .catch(err => {
+    res.status(500).json({success: false, err});
+  });
+});
 
 
