ebe849f6139cf01aa3a75569b8664f9ec5a99443
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-03-15 19:41:05 -0700

    created put endpoint and get by id endpoint. Still testing in Postman

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const server = express();
 
 server.use(express.json());
 
-module.exports = server; 
 
 server.get('/', (req, res) => {
     res.send('hello world!');
   });
   
   
-  server.get('/users', (req, res) => {
+  server.get('/api/users', (req, res) => {
     db.find()
     .then(users => {
       res.status(200).json(users);
@@ -22,19 +21,38 @@ server.get('/', (req, res) => {
       res.status(500).json({success: false, err: "The users information could not be retrieved."});
     });
   })
+  //Get endpoint works in Postman
   
-  server.post('/users', (req, res) => {
+  server.post('/api/users', (req, res) => {
     const userInfo = req.body;
     console.log('body:', userInfo);
   
-    db.add(userInfo)
+    db.insert(userInfo)
     .then((user) => {
-      res.status(201).json({ success: true, user});
+      res.status(200).json({ success: true, user});
       })
       .catch((err) => {
-        res.status(500).json({sucess: false, err});
+        res.status(400).json({success: false, message: 'Please provide name and bio for the user'});
     });
   });
+
+  //Unsure if Post endpoint in working Postman
+
+  server.get('/api/users/:id', (req, res) => {
+      db.findById(req.params.id)
+      .then(user => {
+          if(user) {
+              res.status(200).json({success: true, user});
+          } else {
+              res.status(404).json({success: false, message: 'The user with the specified ID does not exist.'})
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ success: false, message: 'The user information could not be retrieved.'})
+      });
+  });
+
+  //Get user based on id works in Postman
   
   server.delete('/users/:id', (req, res) => {
     const { id } = req.params;
@@ -52,3 +70,29 @@ server.get('/', (req, res) => {
       res.status(500).json({success: false, err});
     });
   });
+
+  //Delete endpoint works in Postman
+
+  server.put('/api/users/:id', (req, res) => {
+      const { name, bio } = req.body;
+
+      if (!name || !bio) {
+          res.status(400).json({errorMessage: 'Please provide name and bio for the user.'});
+      } else {
+          db.update(req.params.id, req.body)
+          .then(user => {
+              if (user) {
+                  res.status(200).json({success: true, user});
+              } else {
+                  res.status(404).json({success: false, message: 'The user with the specified ID does not exist.'});
+              }
+          })
+          .catch(err => {
+              res.status(500).json({success: false, errorMessage: 'The user information cannot be modified.'});
+          })
+      }
+  })
+
+  //Unsure if Put endpoint works on Postman
+
+  module.exports = server; 
